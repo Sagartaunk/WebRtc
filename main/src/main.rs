1326e5362c981c7d3dcede45b2e7a38ca716c2c2
Sagar Taunk <sagartaunk2@gmail.com>
2025-04-10 21:35:28 +0530

    Version = Beta

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,4 +1,4 @@
-use auth::{ServerMiddleware , auth::{login , register ,  key_gen , credentials_gen}};
+use auth::{servermiddleware , auth::{login , register ,  key_gen , file_gen}};
 use actix_web::{App, HttpServer, middleware, web};
 use env_logger;
 use log;
@@ -9,7 +9,7 @@ use local_ip_address::local_ip;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     key_gen();
-    credentials_gen();
+    file_gen(String::from("credentials.json"));
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     let device_ip = local_ip().unwrap();
     let bind_address = format!("{}:6969", device_ip);
@@ -19,7 +19,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .service(
                 web::scope("/api")
-                    .wrap(ServerMiddleware::Middleware)
+                    .wrap(servermiddleware::Middleware)
                     .service(web::resource("/test").to(|| async { "Works?" })) //Place Holder to test
             )
             .route("/register", web::post().to(register))
